62f6f8426968d65a884c88bb1389bf9cc0389bc3
nikhildwivedi329-debug <nikhildwivedi329@gmail.com>
2026-03-14 20:13:33 +0530

    Update DBUtil.java

--- src/main/java/com/tms/util/DBUtil.java
+++ src/main/java/com/tms/util/DBUtil.java
@@ -2,6 +2,7 @@ package com.tms.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.net.URI;
 
 public class DBUtil {
 
@@ -17,13 +18,22 @@ public class DBUtil {
 
         String dbUrl = System.getenv("DATABASE_URL");
 
+
         if (dbUrl == null) {
-            dbUrl = "jdbc:mysql://localhost:3306/tourism_db_dzq6";
-            return DriverManager.getConnection(dbUrl, "root", "Sand@#14");
+            return DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/tourism_db_dzq6",
+                    "root",
+                    "Sand@#14"
+            );
         }
 
-        dbUrl = dbUrl.replace("mysql://", "jdbc:mysql://");
+        URI uri = new URI(dbUrl);
 
-        return DriverManager.getConnection(dbUrl);
+        String user = uri.getUserInfo().split(":")[0];
+        String password = uri.getUserInfo().split(":")[1];
+
+        String jdbcUrl = "jdbc:mysql://" + uri.getHost() + ":" + uri.getPort() + uri.getPath();
+
+        return DriverManager.getConnection(jdbcUrl, user, password);
     }
 }
